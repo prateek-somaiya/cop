0f5fe1178d41f3a837eb91a9a10688c0f8ee05cd
Prateek Somaiya <prateek.somaiya@gmail.com>
2021-04-07 16:55:42 -0500

    changed dist to bin

---
+++ bin/dist/config.d.ts
@@ -0,0 +1,7 @@
+export var __esModule: boolean;
+declare namespace _default {
+    export const maxConcurrency: number;
+    export const defaultRegion: string;
+    export const allRegions: string[];
+}
+export default _default;
---
+++ bin/dist/xlsx-util.d.ts
@@ -0,0 +1,5 @@
+export var __esModule: boolean;
+declare namespace _default {
+    export function instanceRecommendationsToXlsx(filepath: any, recommendations: any): void;
+}
+export default _default;
